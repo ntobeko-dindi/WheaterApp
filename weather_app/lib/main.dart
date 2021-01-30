4875b2c8e72125dd9f8bdfe3bf89888b5b086e90
Ntobeko <47077265+xcoding101@users.noreply.github.com>
2021-01-30 22:58:58 +0200

    second commit
    bugs:
    
    1. reload is done manually

--- weather_app/lib/main.dart
+++ weather_app/lib/main.dart
@@ -1,117 +1,373 @@
+import 'dart:convert';
+import 'dart:ui';
+import 'package:geolocator/geolocator.dart';
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+
+
+TextEditingController searchController = new TextEditingController();
+var seachedByCity = false;
+var apiKey = '4e690fe028875d0bdf8fccf547643e0d';
+var description;
+var maxTemp;
+var minTemp;
+var temp;
+var pressure;
+var humidity;
+var cityName;
+var country;
+var windSpeed;
+var cityNameCtrl;
+var count = 1;
+var icon_url;
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    fetchData();
+    fetchData();
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+        backgroundColor: Colors.transparent,
+        title: TextField(
+          controller: searchController,
+          style: TextStyle(fontSize: 18, fontFamily: 'Lato-Bold'),
+          decoration: InputDecoration(
+            prefixIcon: IconButton(
+              onPressed: () {
+                setState(() {
+                  getWeatherByCity();
+                });
+              },
+              icon: Icon(
+                Icons.search,
+                size: 26,
+                color: Colors.black,
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            contentPadding: const EdgeInsets.symmetric(vertical: 0),
+            border: OutlineInputBorder(
+              borderRadius: const BorderRadius.all(
+                const Radius.circular(60.0),
+              ),
             ),
-          ],
+            filled: true,
+            hintStyle: TextStyle(
+              color: Colors.grey[800],
+              fontFamily: 'Lato-Bold',
+              fontSize: 18,
+            ),
+            hintText: "Search by a city",
+            fillColor: Colors.white70,
+          ),
         ),
+        elevation: 0.0,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.menu,
+              size: 26.0,
+            ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Container(
+        child: Stack(children: [
+          Image.asset(
+            'assets/images/rainy.jpg',
+            fit: BoxFit.cover,
+            height: double.infinity,
+            width: double.infinity,
+          ),
+          Container(
+            decoration: BoxDecoration(color: Colors.black.withOpacity(0.6)),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 85),
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaY: 2, sigmaX: 2),
+              child: Container(
+                child: Scaffold(
+                  backgroundColor: Colors.transparent,
+                  body: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Column(
+                        children: <Widget>[
+                          Container(
+                            child: Center(
+                              child: Text(
+                                '$cityName , $country',
+                                style: TextStyle(
+                                  fontSize: 25,
+                                  color: Colors.white,
+                                  fontFamily: 'Lato-Bold',
+                                ),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            child: IconButton(
+                              onPressed: () {
+                              },
+                              icon: Icon(Icons.location_on),
+                              iconSize: 40,
+                              color: Colors.white,
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(top: 10),
+                            child: Center(
+                              child: Text(
+                                '$temp' + '°',
+                                style: TextStyle(
+                                    fontSize: 40,
+                                    color: Colors.white,
+                                    fontFamily: 'Lato-Bold'),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Container(
+                        child: Icon(
+                          Icons.wb_cloudy_outlined,
+                          color: Colors.white,
+                          size: 100,
+                        ),
+                      ),
+                      Container(
+                        child: Center(
+                          child: Text(
+                            '$minTemp ° | $maxTemp °  $description',
+                            style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.white,
+                                fontFamily: 'Lato-Bold'),
+                          ),
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Container(
+                            child: Column(
+                              children: [
+                                Text(
+                                  'pressure',
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    color: Colors.white,
+                                    fontFamily: 'Lato-Bold',
+                                  ),
+                                ),
+                                Text(
+                                  '$pressure',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.white,
+                                    fontFamily: 'Lato-Bold',
+                                  ),
+                                ),
+                                Text(
+                                  'mb',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.white,
+                                    fontFamily: 'Lato-Bold',
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Container(
+                            child: Column(
+                              children: [
+                                Text(
+                                  'wind',
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    color: Colors.white,
+                                    fontFamily: 'Lato-Bold',
+                                  ),
+                                ),
+                                Text(
+                                  '$windSpeed',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.white,
+                                    fontFamily: 'Lato-Bold',
+                                  ),
+                                ),
+                                Text(
+                                  'km/h',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.white,
+                                    fontFamily: 'Lato-Bold',
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Container(
+                            child: Column(
+                              children: [
+                                Text(
+                                  'humidity',
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    color: Colors.white,
+                                    fontFamily: 'Lato-Bold',
+                                  ),
+                                ),
+                                Text(
+                                  '$humidity ',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.white,
+                                    fontFamily: 'Lato-Bold',
+                                  ),
+                                ),
+                                Text(
+                                  '%',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.white,
+                                    fontFamily: 'Lato-Bold',
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      Container(
+                        child: FlatButton(
+                          child: Icon(
+                            Icons.refresh,
+                            color: Colors.white,
+                            size: 40,
+                          ),
+                          color: Colors.transparent,
+                          onPressed: () {
+                            setState(
+                              () {
+                                fetchData();
+                                fetchData();
+                              },
+                            );
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ]),
+      ),
     );
   }
+}
+
+
+double longitude = 31.044965599999998;
+double latitude = -29.8436578;
+
+Future<void> fetchData() async {
+  var searchTxt = searchController.text.toLowerCase();
+  var apiUrl =
+      'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey&units=metric';
+  if (seachedByCity) {
+    apiUrl =
+        'https://api.openweathermap.org/data/2.5/weather?q=$searchTxt&appid=$apiKey&units=metric';
+    seachedByCity = false;
+  } else {
+    await getCurrentUserLocation();
+  }
+  http.Response response = await http.get(apiUrl);
+  if (response.statusCode == 200) {
+    String data = response.body;
+    var decodedData = jsonDecode(data);
+
+    description = decodedData['weather'][0]['description'];
+    temp = decodedData['main']['temp'];
+    maxTemp = decodedData['main']['temp_max'];
+    minTemp = decodedData['main']['temp_min'];
+    pressure = decodedData['main']['pressure'];
+    humidity = decodedData['main']['humidity'];
+    cityName = decodedData['name'];
+    windSpeed = decodedData['wind']['speed'];
+    country = decodedData['sys']['country'];
+
+  } else {
+    print(response.statusCode);
+  }
+}
+
+void getCurrentUserLocation() async {
+  var position = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.low);
+
+  latitude = position.latitude;
+  longitude = position.longitude;
+
+  print("latitude : " +
+      latitude.toString() +
+      "\n longitude : " +
+      longitude.toString());
+}
+
+getWeatherByCity() {
+  seachedByCity = true;
+  fetchData();
+}
+
+
+showAlertDialog(BuildContext context) {
+
+  // set up the buttons
+  Widget okButton = FlatButton(
+    child: Text("okay!"),
+    onPressed:  () {
+      fetchData();
+    },
+  );
+
+
+  // set up the AlertDialog
+  AlertDialog alert = AlertDialog(
+    title: Text("Error"),
+    content: Text("Please refresh the page!"),
+    actions: [
+      okButton,
+    ],
+  );
+
+  // show the dialog
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return alert;
+    },
+  );
 }
